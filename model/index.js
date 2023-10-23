df8dc08ce1264497616252ab37527efbcf469d71
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-10-22 19:56:21 -0700

    Model/index.js -> use the modules in other parts of the application.

---
+++ model/index.js
@@ -0,0 +1,6 @@
+// Import the modules
+const User = require("./User"); // Import the User module.
+const Thought = require("./Thought"); // Import the Thought module.
+
+// Export both modules to use
+module.exports = { User, Thought }; // Export User and Thought as an object.
